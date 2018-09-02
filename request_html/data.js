da709b88582c5e9a17a567684e1a1c88cbef1802
Your Name <you@example.com>
2018-09-02 18:20:05 +0900

    new

---
+++ request_html/data.js
@@ -0,0 +1,27 @@
+var conf_data = [
+    {
+        tab_name: "google",
+        desc: "google検索",
+        url:"http://google.co.jp/search",
+        params:[
+            {name:"q", value:"HTML CSS"},
+            {name:"en", value:""},
+        ]
+    },
+    {
+        tab_name: "yahoo",
+        desc: "yahoo検索",
+        url:"https://search.yahoo.co.jp/search",
+        params:[
+            {name:"p", value:"HTML CSS"},
+        ]
+    },
+    {
+        tab_name: "bing",
+        desc: "bing検索",
+        url:"https://www.bing.com/search",
+        params:[
+            {name:"q", value:"HTML CSS"},
+        ]
+    },
+];
---
+++ request_html/main.js
@@ -0,0 +1,111 @@
+var sel_id = 0;
+
+// テキストボックスの値をJSONに保存
+function save_input_data(html_dat, json_dat, key_name){
+    console.log("in save_data:"+ json_dat[key_name]);
+    html_dat.addEventListener("change", function(){
+        console.log("JSON:" + json_dat[key_name] + " HTML:" + html_dat.value);
+        json_dat[key_name] = html_dat.value;
+    },false);
+};
+
+// タブクリック時の処理
+function click_tab(id){
+    // タブのスタイルを変更
+    var selected_menu_id = "menu_id_" + sel_id;
+    var menu_id = "menu_id_" + id;
+    console.log("id:" + menu_id);
+    var menu = document.getElementById(menu_id);
+    document.getElementById(selected_menu_id).style.backgroundColor = "";
+    var menu = document.getElementById(menu_id);
+    menu.style.backgroundColor = "grey";
+
+    // URLを設定
+    var url = conf_data[id].url;
+    document.getElementById("url").value = url;
+    sel_id = id;
+
+    // パラメータを設定
+    var param_list, params, param, param_dat;
+    param_list = document.getElementById("param_list");
+    while(param_list.lastChild){
+        param_list.removeChild(param_list.lastChild);
+    }
+    params = conf_data[id].params;
+    for(var i=0; i<params.length; i++){
+        console.log("name:" + params[i].name + " data:" + params[i].value);
+        var param = document.createElement("div");
+        param.className = "param_item";
+
+        var param_dat;
+        // パラメータ名を作成
+        param_dat = document.createElement("div");
+        param_dat.className = "param_name";
+        param_dat.classList.add("label");
+        param_dat.innerText = params[i].name;
+        param.appendChild(param_dat);
+
+        // パラメータの値を作成
+        param_dat = document.createElement("input");
+        param_dat.className = "param_data";
+        param_dat.type = "text";
+        param_dat.name = params[i].name;
+        param_dat.value = params[i].value;
+        // save_input_data(param_dat, params[i], "value");
+        (function(html_dat, json_dat){
+            html_dat.addEventListener("change", function(){
+                console.log("JSON:" + json_dat.value+ " HTML:" + html_dat.value);
+                json_dat.value = html_dat.value;
+            },false);
+        })(param_dat, params[i]);
+        param.appendChild(param_dat);
+        param_list.appendChild(param);
+    }
+};
+
+// URLの値を保存
+var html_url = document.getElementById("url");
+html_url.addEventListener("change", function(){
+    console.log("sel_id:" + sel_id);
+    console.log("JSON:" + conf_data[sel_id].url + " HTML:" + html_url.value);
+    conf_data[sel_id].url = html_url.value;
+},false);
+
+// HTTPリクエスト
+function request(method){
+    var url = document.getElementById("url").value
+    console.log(url);
+    document.getElementById("form_id").setAttribute("action", url);
+    document.getElementById("form_id").setAttribute("method", method);
+    document.getElementById("form_id").submit();
+}
+
+// GET リクエスト
+function get(){
+    request("GET");
+}
+
+// POST リクエスト
+function post(){
+    request("POST");
+}
+
+// 初期処理(タブ作成)
+var elem;
+var menu_bar = document.getElementById("menu_bar");
+for(var i=0; i<conf_data.length; i++){
+    elem = document.createElement("div");
+    elem.id = "menu_id_" + i;
+    elem.className = "menu_item";
+    elem.innerText = conf_data[i].tab_name;
+    menu_bar.appendChild(elem);    
+    // console.log(conf_data[i].tab_name);
+    (function(elem_, id){
+        elem.addEventListener("click", function(){
+            click_tab(id);
+        }, false);
+    })(elem, i);
+}
+
+// 最初のタブを選択
+click_tab(0);
